0b6f784acb89ac4ca3e79059a52165da7d52de9d
Linyi1707 <117964958+Linyi1707@users.noreply.github.com>
2023-02-20 11:33:01 +0800

    无

---
+++ Hello_World.java
@@ -0,0 +1,8 @@
+public class Hello_World { //这是一个Java练习
+        public static void main(String[] args) {
+            System.out.println("姓名：李子明");
+            System.out.print("性别：男\n");
+            System.out.print("年龄：18");
+        }
+
+}
