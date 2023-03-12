e1a9e95bb43adf508a07e3900787f398e65ca1f8
Linyi1707 <117964958+Sirtey@users.noreply.github.com>
2023-03-12 17:29:59 +0800

    3.12

--- Java_test/test_3/src/day_8/test_2.java
+++ Java_test/test_3/src/day_8/test_2.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 
 public class test_2 {
     public static void main(String[] args) {
-        Scanner run = new Scanner(System.in);
-        System.out.print("输入分数：");
-        if (run.hasNextInt()) {
-            int cgj = run.nextInt();
-            if (cgj == 100) {
-                System.out.println("买车");
-            } else if (cgj >= 90) {
-                System.out.println("买MP4");
-            } else if (cgj >= 60) {
-                System.out.println("买参考书");
+        try (Scanner run = new Scanner(System.in)) {
+            System.out.print("输入分数：");
+            if (run.hasNextInt()) {
+                int cgj = run.nextInt();
+                if (cgj == 100) {
+                    System.out.println("买车");
+                } else if (cgj >= 90) {
+                    System.out.println("买MP4");
+                } else if (cgj >= 60) {
+                    System.out.println("买参考书");
+                } else {
+                    System.out.println("好好学习");
+                }
             } else {
-                System.out.println("好好学习");
+                System.out.println("输入错误!");
             }
-        } else {
-            System.out.println("输入错误!");
         }
     }
 }
--- Java_test/test_3/src/day_8/test_3.java
+++ Java_test/test_3/src/day_8/test_3.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
 
 public class test_3 {
     public static void main(String[] args) {
-        Scanner run = new Scanner(System.in);
-        System.out.print("成绩秒数：");
-        if (run.hasNextInt()) {
-            int cge = run.nextInt();
-            if (cge < 10) {
-                System.out.println("进入决赛，请选择性别：");
-                String xuz = run.next();
-                if (xuz.equals("男")) {
-                    System.out.print("已进入男子组");
-                } else if (xuz.equals("女")) {
-                    System.out.print("已进入女子组");
-                } else {
-                    System.out.print("输入错误！");
-                }
+        try (Scanner run = new Scanner(System.in)) {
+            System.out.print("成绩秒数：");
+            if (run.hasNextInt()) {
+                int cge = run.nextInt();
+                if (cge < 10) {
+                    System.out.println("进入决赛，请选择性别：");
+                    String xuz = run.next();
+                    if (xuz.equals("男")) {
+                        System.out.print("已进入男子组");
+                    } else if (xuz.equals("女")) {
+                        System.out.print("已进入女子组");
+                    } else {
+                        System.out.print("输入错误！");
+                    }
 
+                } else {
+                    System.out.print("比赛结束。");
+                }
             } else {
-                System.out.print("比赛结束。");
+                System.out.print("输入错误！");
             }
-        } else {
-            System.out.print("输入错误！");
         }
     }
 }
