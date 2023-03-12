e79a1e52c9e6b565f2056ed5cdeffdff3471340a
Linyi1707 <117964958+Sirtey@users.noreply.github.com>
2023-03-12 17:30:12 +0800

    3.12

--- Java_test/test_3/src/day_8/test_1.java
+++ Java_test/test_3/src/day_8/test_1.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class test_1 {
     public static void main(String[] args) {
-        Scanner run = new Scanner(System.in);
-        System.out.print("中没中：");
-        if (run.hasNext()) {
-            String cip = run.next();
-            switch (cip) {
-                case "中" -> System.out.println("买车");
-                case "没中" -> System.out.println("接着买");
-                default -> System.out.println("输入错误！");
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("中没中：");
+            if (in.hasNext()) {
+                String cip = in.next();
+                switch (cip) {
+                    case "中" -> System.out.println("买车");
+                    case "没中" -> System.out.println("接着买");
+                    default -> System.out.println("输入错误！");
+                }
+            } else {
+                System.out.println("输入错误！");
             }
-        } else {
-            System.out.println("输入错误！");
         }
     }
 }
