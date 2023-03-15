eec44ba8dfa2b2299e5ba849f4cef94057fd647c
Linyi1707 <117964958+Sirtey@users.noreply.github.com>
2023-03-15 17:14:43 +0800

    3.15

--- Java_test/test_3/src/day_15/test_3_1.java
+++ Java_test/test_3/src/day_15/test_3_1.java
@@ -2,6 +2,15 @@ package day_15;
 
 public class test_3_1 {
     public static void main(String[] args) {
+        String jie = "";
+        String jig = "*";
+        String jeg = "";
+        for (int j = 1; j < 6; j++) {
+            jie += " ";
+            jeg += jie + jig + "\n";
+            jig += "**";
+           } 
+        System.out.print(jeg);
 
     }
 }
