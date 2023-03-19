2fd49ef9ea22467c8cdb4f5a56b2f656362ffdfc
Linyi1707 <117964958+Sirtey@users.noreply.github.com>
2023-03-19 14:22:16 +0800

    3.19

--- Java_test/test_3/src/Java_test/test_3/src/day_8/test1.java
+++
@@ -1,7 +0,0 @@
-package Java_test.test_3.src.day_8;
-
-public class test1 {
-    public static void main(String[] args) {
-        System.out.println("hello,world");
-    }
-}
---
+++ Java_test/test_3/src/day_19/Person.java
@@ -0,0 +1,7 @@
+package day_19;
+
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("你好！");
+    }
+}
