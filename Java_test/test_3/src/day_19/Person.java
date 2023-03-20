1d594f847ff3c912e4159cdc9a2c9bd29721aef1
Linyi1707 <117964958+Sirtey@users.noreply.github.com>
2023-03-20 12:39:46 +0800

    3.20

--- Java_test/test_3/src/day_19/Person.java
+++
@@ -1,7 +0,0 @@
-package day_19;
-
-public class Person {
-    public static void main(String[] args) {
-        System.out.println("你好！");
-    }
-}
--- Java_test/test_3/src/day_19/type.java
+++ Java_test/test_3/src/day_19/type.java
@@ -1,5 +1,7 @@
 package day_19;
 
 public class type {
-
+    public static void main(String[] args) {
+        int[] sum = {}; 
+    }
 }
