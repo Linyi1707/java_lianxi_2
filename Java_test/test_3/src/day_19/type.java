dc4f6190c16a2c2e7d67da31a6d86f565d832a88
Linyi1707 <117964958+Sirtey@users.noreply.github.com>
2023-03-20 13:25:01 +0800

    3.20

--- Java_test/test_3/src/day_19/type.java
+++
@@ -1,7 +0,0 @@
-package day_19;
-
-public class type {
-    public static void main(String[] args) {
-        int[] sum = {}; 
-    }
-}
---
+++ Java_test/test_3/src/day_20/test_1.java
@@ -0,0 +1,22 @@
+package day_20;
+
+import java.util.Scanner;
+
+public class test_1 {
+    public static void main(String[] args) {
+        int[] list = {8, 4, 2, 1, 23, 344, 12};
+        int sum = 0;
+        try (Scanner run = new Scanner(System.in)) {
+            System.out.print("请输入所猜号码：");
+                int data = run.nextInt();
+            for (int i = 0; i < list.length; i++) {
+                System.out.println(list[i]);
+                if (list[i] == data) {
+                    System.out.println("猜对了");
+                }
+                sum += list[i];
+            }
+        }
+        System.out.println("所有元素的和为：" + sum);
+    }
+}
