daa683b76538849adbddb6afd46e66592bd54c0c
Linyi1707 <117964958+Sirtey@users.noreply.github.com>
2023-04-03 13:35:18 +0800

    4.3

--- Java_test/April/test.java
+++ Java_test/April/test.java
@@ -1,9 +1,44 @@
 package Java_test.April;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        Random
+        String k1 = "*******************开始游戏****************";
+        String k2 = "*******************结束游戏****************";
+        System.out.print(k1);
+        Random run = new Random();
+        Scanner in = new Scanner(System.in);
+        System.out.print("请输入您的基础金额：");
+        int money = in.nextInt();
+        if (money >= 100000000) {
+            System.out.print("请输入合理的数值(不要超过一亿)！");
+        } else {
+            System.out.print("您现在的余额为" + money);
+        }
+        while (true) {
+            int[] list = new int[10];
+            for (int i = 0; i < list.length; i++) {
+                int sum = run.nextInt(9);
+                list[i] = sum;
+            } while (true) {
+                int data1 = money;
+                if (money <= 0) {
+                    System.out.print("余额过少，请充值：");
+                    money = in.nextInt();
+                } else {
+                    System.out.print("请输入想要投入的金额：");
+                    int data3 = in.nextInt();
+                    int jiang = data3;
+                    money = money - jiang;
+                    jiang = jiang * 2 / 3;
+                    int data2 = jiang;
+                    System.out.print("您现在的余额为" + money);
+                    break;
+                } if ()
+            }
+
+        }
     }
 }
